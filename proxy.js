a4f6791bb9114e866fdf4894758810667b5044f8
Wanghao <wanghao1891@126.com>
2015-01-02 09:32:12 +0800

    init

--- proxy.js
+++ proxy.js
@@ -2,7 +2,7 @@ var http = require('http');
 var exec = require("child_process").exec;
 
 http.createServer(function (req, res) {
-  exec("petite --script /root/workspace/database/base.ss", function (error, stdout, stderr) {
+  exec("cd /root/workspace/database/; petite --script get.ss", function (error, stdout, stderr) {
     var content = stdout;
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end(content + '\n');
