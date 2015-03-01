74747b9b6b079c8f96d8936f0201cc9c25f21703
Wanghao <wanghao1891@126.com>
2015-03-01 20:50:12 +0800

    format the definition of vocabulary.

--- get.js
+++ get.js
@@ -65,14 +65,16 @@ function getRelative(_value) {
 
 	    var _length = _list.length;
 
-	    var relativeList = "<ul>"
+	    //var relativeList = "<ul>"
+	    var relativeList = "";
 
 	    for (i=0; i< _length; i++) {
 		var _row = _list[i].searchtext;
-		relativeList += "<li><a onclick='searchRelative(\"" + _row + "\")'>" + _row + "</a></li>";
+//		relativeList += "<li><a onclick='searchRelative(\"" + _row + "\")'>" + _row + "</a></li>";
+		relativeList += "<a onclick='searchRelative(\"" + _row + "\")'>" + _row + "</a></br>";
 	    }
 
-	    relativeList += "</ul>"
+	    //relativeList += "</ul>"
 
 	    relativeDiv.innerHTML = relativeList;
 
@@ -97,66 +99,8 @@ function query(event) {
 
     if (event.keyCode == 13) {
 	search();
-	
-/*	var searchText = "search?key=" + _value;
-	//searchTextField.value = "";
-	
-	console.log(searchText);
-
-	var req = new XMLHttpRequest();
-	req.onreadystatechange = function() {
-	    if (req.readyState == 4 && req.status == 200) {
-		searchTextDiv.innerHTML = "The search of " + _value + " is finished."
-
-		if (JSON.parse(req.responseText) === 0) {
-		    getVocabulary();
-		} else {
-		    _id = _value + "-li";
-		    _element = document.getElementById(_id);
-		    _element.style.color = "blue";
-		    _element.style.fontStyle = "italic";
-		    location.hash = _id;
-		}
-
-		searchTextField.value = "";
-	    }
-	}
-
-	req.open("GET", searchText, true);
-	req.send();
-	
-	searchTextDiv.innerHTML = "Searching for " + searchTextField.value + " ...";*/
     } else if (_value.length > 1) {
 	getRelative(_value);
-	
-/*	console.log("Query relative.");
-	var _query = "relative?key=" + _value; 
-	var req = new XMLHttpRequest();
-	
-	var relativeDiv = document.getElementById("relative_div");
-
-	req.onreadystatechange = function() {
-	    if (req.readyState == 4 && req.status == 200) {
-		var _list = JSON.parse(req.responseText).results;
-
-		var _length = _list.length;
-
-		var relativeList = "<ul>"
-		
-		for (i=0; i< _length; i++) {
-		    relativeList += "<li>" + _list[i].searchtext + "</li>";
-		}
-
-		relativeList += "</ul>"
-
-		relativeDiv.innerHTML = relativeList;
-
-		relativeDiv.hidden = false;
-	    }
-	}
-
-	req.open("GET", _query, true);
-	req.send();*/
     }
 }
 
@@ -188,6 +132,9 @@ function getVocabulary() {
 		pronunciation_us = decodeURIComponent(vocabulary.pronunciation_us);
 		sound_us = decodeURIComponent(vocabulary.sound_us);
 		definition = decodeURIComponent(vocabulary.definition);
+
+		//process definition for format.
+		definition = definition.replace(/\./g, ".<p>");
 
 		content += "<li><a id='" + name + "-li' onmouseover=\"play('" + sound_uk + "')\" onclick=\"getVocabularyDetail('" + name  +  "')\">" + name + "</a>" + " [<a onclick=\"play('" + sound_uk  + "')\">BrE</a> /" + pronunciation_uk  + "/&nbsp<a onclick=\"play('" + sound_us + "')\">NAmE</a> /" + pronunciation_us  + "/]</li>";
 		content += "<div id='" + name + "' style='border:1px solid #000' hidden='true'>" + definition  + "</div>"
